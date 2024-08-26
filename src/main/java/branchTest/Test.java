26db735c2842d94f0887f7fd1fe1bce8d5cc3a00
iam_dcastro <iam_dcastro@contractor.indeed.com>
2024-08-26 12:33:34 -0500

    testing CI

--- src/main/java/branchTest/Test.java
+++ src/main/java/branchTest/Test.java
@@ -6,7 +6,7 @@ public class Test
     public static void main(String[] args)
     {
 
-        System.out.println("hello camilo since my test in java");
+        System.out.println("testing CI");
 
     }
 }
