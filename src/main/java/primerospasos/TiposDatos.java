c19ecf0b99d54ddf35340ba07502efcb314556c9
unknown <camiloospina1133@gmail.com>
2024-08-27 16:58:25 -0500

    Tipos de datos

---
+++ src/main/java/primerospasos/TiposDatos.java
@@ -0,0 +1,17 @@
+package primerospasos;
+
+public class TiposDatos {
+    public static void main(String[]args){
+        byte b=127;
+        char c='a';
+        short s=32767;
+        long l=9223372036854775807L;
+        float f=3.14f;
+        double d=3.14;
+        boolean b1=true;
+        boolean b2=false;
+
+        System.out.println("Los tipos de datos de tipo ENTEROS en java son: "  + b + " " + s + " " + l + " " + f + " " + d );
+
+    }
+}
